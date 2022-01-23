2dfe9bb5e7a58e23242826f16a1d4fc06b523bdd
Rihan Mohammed <85993821+rihanmohammed26@users.noreply.github.com>
2022-01-23 23:49:50 +0530

    completed

---
+++ src/Banner.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import requests from "./requests";
+import instance from "./axios";
+import "./banner.css";
+
+function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const req = await instance.get(requests.fetchNetflixOriginals);
+      //   console.log(
+      //     req.data.results[
+      //       Math.floor(Math.random() * (req.data.resuls.length - 1))
+      //     ]
+      //   );
+      setMovie(
+        req.data.results[
+          Math.floor(Math.random() * req.data.results.length - 1)
+        ]
+      );
+      return req;
+    }
+    fetchData();
+  }, []);
+  // console.log(movie);
+
+  function trunCate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="bannner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{trunCate(movie?.overview, 150)}</h1>
+      </div>
+      <div className="banner_fadeBottom"></div>
+    </header>
+  );
+}
+
+export default Banner;
+
+// 1.55.50
--- src/Row.js
+++ src/Row.js
@@ -1,23 +1,53 @@
+import movieTrailer from "movie-trailer";
 import React, { useEffect, useState } from "react";
+import YouTube from "react-youtube";
 import instance from "./axios";
 import "./row.css";
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+
+  const [trailerUrl, setTrailerUrl] = useState();
+
   const base_url = "https://image.tmdb.org/t/p/original/";
 
   useEffect(() => {
     // if [], run once when the row loads and dont run again
     async function fetchData() {
       const req = await instance.get(fetchUrl); // instance.get("/api") looking like "https://api.themoviedb.org/3/api".
-      //   console.log(req.data.results);
+      // console.log(req);
+      // console.log(req.data.results);
       setMovies(req.data.results);
       return req;
     }
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  // console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+      playsinline: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search); //https://www.youtube.com/watch?v=FDJDFNFDHR searching like this
+          console.log(setTrailerUrl(urlParams.get("v")));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
   return (
     <div className="row">
@@ -27,12 +57,17 @@ function Row({ title, fetchUrl }) {
         {/* Poster Container */}
         {movies.map((movie) => (
           <img
-            className="row_poster"
-            src={`${base_url}${movie.poster_path}`}
+            onClick={() => handleClick(movie)}
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           /> //gettting all the movie poster
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
